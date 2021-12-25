768ea3f4d25dc4960fa83ca4fc255a6882f43414
gnahzeus <zhangyue1689@gmail.com>
2021-12-25 22:07:10 +0800

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import React, { Component } from "react";
 import './App.css';
 import TasksContainer from "./components/TasksContainer";
+import Task from "./components/Task";
+import Form from "./components/Form";
+import FilterTag from "./components/FilterTag";
 
+function App(props) {
+  const taskList = props.tasks?.map(task => ( 
+      <Task 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed}
+        key={task.id}
+      />));
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>My Task Manager</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            Add New Task
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+/*
 class App extends Component {
   render() {
     return (
@@ -14,26 +77,6 @@ class App extends Component {
     );
   }
 }
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 */
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+const TASKS = [
+  { id: "task-0", name: "Eat", completed: true },
+  { id: "task-1", name: "Sleep", completed: false },
+  { id: "task-2", name: "Repeat", completed: false }
+];
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
+ReactDOM.render(<App tasks={TASKS} />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
