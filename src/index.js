535390e3f52694c0f5874d299a267a1c27f0e943
gnahzeus <zhangyue1689@gmail.com>
2021-12-29 10:32:44 +0800

    edit default

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 
 const TASKS = [
-  { id: "task-0", name: "", completed: true },
-  { id: "task-1", name: "", completed: false },
-  { id: "task-2", name: "", completed: false }
+  { id: "task-0", name: ".", completed: true },
+  { id: "task-1", name: ".", completed: false },
+  { id: "task-2", name: ".", completed: false }
 ];
 /*
 ReactDOM.render(
